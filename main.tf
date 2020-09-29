fdadfe914c4c9a2f051769ca7bb1b5762b937658
gopal <gopal>
2020-09-29 03:08:49 -0700

    azure vnet

--- main.tf
+++ main.tf
@@ -19,3 +19,13 @@ resource "azurerm_subnet" "web_server_subnet" {
     virtual_network_name = azurerm_virtual_network.web_server_vnet.name 
     address_prefix = var.web_server_address_prefix
 }
+resource "azurerm_network_interface" "web_server_nic" {
+ name = "${var.web_server_name}-nic"
+ location = var.web_server_location
+ resource_group_name = azurerm_resource_group.web_server_rg.name
+ ip_configuration {
+     name = "${var.web_server_name}-ip"
+     subnet_id = azurerm_subnet.web_server_subnet.id
+     private_ip_address_allocation = "Dynamic"
+ }
+}
