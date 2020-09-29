4f09e0d0058c6a70dd12185451becd123b7c7740
gopal <gopal>
2020-09-29 00:57:50 -0700

    rg with variable

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
     features{}
 }
 resource "azurerm_resource_group" "web_server_rg"{
-    name = "web-reg"
-    location = "westus2"
+    name = var.web_server_rg
+    location = var.web_server_location
 }
