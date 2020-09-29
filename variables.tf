ea8f6cf121f109514eff2b76b9e58dd557f0fa94
gopal <gopal>
2020-09-29 02:19:07 -0700

    azure vnet

--- variables.tf
+++ variables.tf
@@ -4,3 +4,13 @@ variable "web_server_location"{
 variable "web_server_rg" {
     type = string
 }
+variable "resource_prefix"{
+    type = string
+}
+variable "web_server_address_space"{
+    type = string
+}
+
+variable "web_server_address_prefix" {
+
+}
