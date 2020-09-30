39ef96a4df519c195a965dd12df3cf29dd7d325f
gopal <gopal>
2020-09-30 01:56:51 -0700

    terraform vault

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ provider "azurerm" {
     version = "=2.20.0"
     features{}
 }
+locals {
+    
+}
 resource "azurerm_resource_group" "web_server_rg"{
     name = var.web_server_rg
     location = var.web_server_location
@@ -60,13 +63,13 @@ resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp"{
 
 }
 
-resource "azurerm_subnet_network_security_group_association" "web_server_nsg_association"{
-    network_interface_id      = azurerm_network_interface.web_server_nic.id 
+resource "azurerm_subnet_network_security_group_association" "web_server_sag"{
+    network_security_group_id      = azurerm_network_security_group.web_server_nsg.id
   subnet_id = azurerm_subnet.web_server_subnet.id
  
   
 }
-resource "azurerm_windows_virtual_machine" "web_server" {
+resource "azurerm_virtual_machine" "web_server" {
     name = "${var.web_server_name}-${format("%02d",count.index)}"
     location = var.web_server_location
     resource_group_name = azurerm_resource_group.web_server_rg.name 
@@ -74,15 +77,26 @@ resource "azurerm_windows_virtual_machine" "web_server" {
     availability_set_id = azurerm_availability_set.web_server_availability_set.id
     count = var.web_server_count
     size = "Standard_B1s"
-    admin_username = "webserver"
-    admin_password = "Password1234"
     
-    os_disk {
+    
+    
+    storage_profile_os_disk {
         caching = "ReadWrite"
         storage_account_type = "Standard_LRS"
     }
 
-    source_image_reference {
+    os_profile {
+        computer_name = web_server_name
+        admin_username = "webserver"
+        admin_password = data.azurerm_key_vault_secret.admin_password.value
+    }
+    os_profile_windows_config {
+        provision_vm_agent = true
+        enable_automatic_upgrades = true
+    }   
+    
+
+    storage_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2016-Datacenter"
